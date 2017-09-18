d0a72eaf08f7dfe147af2c6cd3e8634ed19889b5
xing <xinghua24@gmail.com>
2017-09-18 01:39:49 -0400

    create a more robust Simple Project that has spring by default

--- SimpleWithSpring/src/main/java/App.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class App {
-	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-	}
-}
