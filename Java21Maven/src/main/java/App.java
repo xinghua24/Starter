e5a1796a1c8884f8e29adbd856b674011859a078
Xinghua Zhang <xinghua24@gmail.com>
2026-03-08 11:19:46 -0400

    Update dependencies in pom.xml and add application start message in App.java

--- Java21Maven/src/main/java/App.java
+++ Java21Maven/src/main/java/App.java
@@ -1,6 +1,10 @@
 
 public class App {
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		System.out.println("Application Started");
+=======
+
+>>>>>>> 8046cc1 (initialize project structure with Maven and add basic configuration files)
 	}
 }
